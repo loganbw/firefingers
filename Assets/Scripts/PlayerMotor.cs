2232b6e49372c9219b67df8fe9f02949f3d32977
loganbw <loganbryantweathersby@gmail.com>
2017-12-03 12:35:40 -0500

    Initial Commit

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour {
+	private CharacterController controller;
+	private Vector3 moveVector;
+	private float verticalVelocity = 0.0f;
+	private float speed = 5.0f;
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+	}
+
+	// Update is called once per frame
+
+	void Update () {
+		moveVector = Vector3.zero;
+		//x = left and right
+		moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+		//y = up and down
+		moveVector.y = verticalVelocity;
+		// z = forward and backward
+		moveVector.z = speed;
+
+		controller.Move (moveVector * Time.deltaTime);
+	}
+}
---
+++ Assets/camreaMotor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camreaMotor : MonoBehaviour {
+	private Transform lookAt;
+	private Vector3 startOffSet;
+	private Vector3 moveVector;
+
+	//for animation at the begining
+	private float transition = 0.0f;
+	private float animationDuration = 2.0f;
+	private Vector3 animationOffSet = new Vector3 (0, 5, 5);
+
+	// Use this for initialization
+	void Start () {
+		lookAt = GameObject.FindGameObjectWithTag ("Player").transform;
+		startOffSet = transform.position - lookAt.position;
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		moveVector = lookAt.position + startOffSet;
+		//locking camrea
+		//x
+		moveVector.x = 0;
+		//y --- for hills exc.
+		//moveVector.y = Mathf.Clamp(moveVector.y,3,5);
+		//z
+		transform.position = moveVector;
+	}
+}
